999d0c7a67b358e31e0e709199f817d2a4d3201d
Victor Uvarov <victorcsc131@gmail.com>
2018-03-17 23:49:24 -0700

    Added NewTaskGui Functionality

--- Task Manager/src/sample/Controller.java
+++ Task Manager/src/sample/Controller.java
@@ -1,17 +1,40 @@
 package sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 public class Controller
 {
     public void newTaskButton()
     {
-
+       openNewTaskWindow();
     }
     public void editTaskButton()
     {
-
+        openNewTaskWindow();
     }
     public void deleteTaskButton()
     {
 
+    }
+    private void openNewTaskWindow()
+    {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("NewTaskMenu.fxml"));
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initStyle(StageStyle.DECORATED);
+            stage.setTitle("Create New Task");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        }
+        catch (Exception e){
+            System.err.println("ERROR: could not open New Task window");
+        }
     }
 }
--- Task Manager/src/sample/Main.java
+++ Task Manager/src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
         primaryStage.setTitle("Task Manager");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
